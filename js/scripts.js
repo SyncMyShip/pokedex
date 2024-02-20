160171a42fea123f72c7a1e0a0740d2478e87a2c
Simba <jadefrancis@JudgeTrudy.local>
2024-02-20 08:52:38 -0500

    create and populate pokemonList variable

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+let pokemonList = [
+    {
+        name: 'Bulbasaur', 
+        hp: 45, 
+        attack: 49, 
+        defense: 49, 
+        speed: 45, 
+        type: [
+            'grass',
+            'poison'
+        ] 
+    },
+    {
+        name: 'Squirtle',
+        hp: 44,
+        attack: 48,
+        defense: 65,
+        speed: 43,
+        type: ['water']
+    },
+    {
+        name: 'Charmander', 
+        hp: 39,
+        attack: 52,
+        defense: 43,
+        speed: 65,
+        type: ['fire']
+    }
+];
