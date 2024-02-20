e68b6a7a57f4a9cb1b3f69c4414f71a7ee22629e
SyncMyShip <77575480+SyncMyShip@users.noreply.github.com>
2024-02-20 08:54:00 -0500

    Section 1.2 (#3)
    * clear scripts.js
    
    * create and populate pokemonList variable
    
    ---------
    
    Co-authored-by: Simba <jadefrancis@JudgeTrudy.local>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,29 @@
-alert('Hello world!');
-
-let favoriteFood = "Ramen";
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Bulbasaur', 
+        hp: 45, 
+        attack: 49, 
+        defense: 49, 
+        speed: 45, 
+        type: [
+            'grass',
+            'poison'
+        ] 
+    },
+    {
+        name: 'Squirtle',
+        hp: 44,
+        attack: 48,
+        defense: 65,
+        speed: 43,
+        type: ['water']
+    },
+    {
+        name: 'Charmander', 
+        hp: 39,
+        attack: 52,
+        defense: 43,
+        speed: 65,
+        type: ['fire']
+    }
+];
