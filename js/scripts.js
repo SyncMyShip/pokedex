9b1c0e2b914047ed51abc18462882c7eb9344b11
SyncMyShip <77575480+SyncMyShip@users.noreply.github.com>
2024-02-29 22:50:33 -0500

    section-1.5: replace for loop with forEach() (#6)
    * replace for loop with forEach()
    
    * refactor using iife
    
    ---------
    
    Co-authored-by: Simba <jfrancis1195@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,55 @@
-let pokemonList = [
-    {
-        name: 'Bulbasaur', 
-        hp: 45, 
-        attack: 49, 
-        defense: 49, 
-        speed: 45, 
-        height: 0.7,
-        types: [
-            'grass',
-            'poison'
-        ] 
-    },
-    {
-        name: 'Squirtle',
-        hp: 44,
-        attack: 48,
-        defense: 65,
-        speed: 43,
-        height: 0.5,
-        types: ['water']
-    },
-    {
-        name: 'Charmander', 
-        hp: 39,
-        attack: 52,
-        defense: 43,
-        speed: 65,
-        height: 0.6,
-        types: ['fire']
+let pokemonRepository = (function () {
+    let pokemonList = [
+      {
+          name: 'Bulbasaur', 
+          hp: 45, 
+          attack: 49, 
+          defense: 49, 
+          speed: 45, 
+          height: 0.7,
+          types: [
+              'grass',
+              'poison'
+          ] 
+      },
+      {
+          name: 'Squirtle',
+          hp: 44,
+          attack: 48,
+          defense: 65,
+          speed: 43,
+          height: 0.5,
+          types: ['water']
+      },
+      {
+          name: 'Charmander', 
+          hp: 39,
+          attack: 52,
+          defense: 43,
+          speed: 65,
+          height: 0.6,
+          types: ['fire']
+      }
+    ]
+    
+    function getAll() {
+      return pokemonList;
     }
-];
-
-
-for (let i = 0; i < pokemonList.length; i++ ) {
-    if (pokemonList[i].height > 0.6) {
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + " - Wow, that's big!" + "<br>")
+    
+    function add(pokemon) {
+      pokemonList.push(item);
+    }
+    
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
+  
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    if (pokemon.height > 0.6) {
+      document.write(pokemon.name + " (height: " + pokemon.height + ")" + " - Wow, that's big!" + "<br>")
     } else {
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br>")
+      document.write(pokemon.name + " (height: " + pokemon.height + ")" + "<br>")
     }
-}
+  })
