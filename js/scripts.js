5d34823934084a55c8bc4c47a799c99228f93f8e
Simba <jfrancis1195@gmail.com>
2024-02-29 22:49:42 -0500

    refactor using iife

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,52 @@
-let pokemonList = [
-    {
-        name: 'Bulbasaur', 
-        hp: 45, 
-        attack: 49, 
-        defense: 49, 
-        speed: 45, 
-        height: 0.7,
-        types: [
-            'grass',
-            'poison'
-        ] 
-    },
-    {
-        name: 'Squirtle',
-        hp: 44,
-        attack: 48,
-        defense: 65,
-        speed: 43,
-        height: 0.5,
-        types: ['water']
-    },
-    {
-        name: 'Charmander', 
-        hp: 39,
-        attack: 52,
-        defense: 43,
-        speed: 65,
-        height: 0.6,
-        types: ['fire']
+let pokemonRepository = (function () {
+    let pokemonList = [
+      {
+          name: 'Bulbasaur', 
+          hp: 45, 
+          attack: 49, 
+          defense: 49, 
+          speed: 45, 
+          height: 0.7,
+          types: [
+              'grass',
+              'poison'
+          ] 
+      },
+      {
+          name: 'Squirtle',
+          hp: 44,
+          attack: 48,
+          defense: 65,
+          speed: 43,
+          height: 0.5,
+          types: ['water']
+      },
+      {
+          name: 'Charmander', 
+          hp: 39,
+          attack: 52,
+          defense: 43,
+          speed: 65,
+          height: 0.6,
+          types: ['fire']
+      }
+    ]
+    
+    function getAll() {
+      return pokemonList;
     }
-];
-
-// writes the name of the Pokemon && appends a string if height is > 0.6
-pokemonList.forEach(function(pokemon) {
+    
+    function add(pokemon) {
+      pokemonList.push(item);
+    }
+    
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
+  
+  pokemonRepository.getAll().forEach(function(pokemon) {
     if (pokemon.height > 0.6) {
       document.write(pokemon.name + " (height: " + pokemon.height + ")" + " - Wow, that's big!" + "<br>")
     } else {
